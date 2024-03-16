5c2888fa1a359694817bedbd3e731a7297683ce9
PiotrWeppo <piotr.weppo@gmail.com>
2024-03-16 21:34:41 +0100

    Finding and choosing video files in program directory

---
+++ files_operations.py
@@ -0,0 +1,31 @@
+import glob
+import os
+
+
+def finding_video_file():
+    types = ("*.mp4", "*.mov")
+    files_grabbed = []
+    for files in types:
+        files_grabbed.extend(glob.glob(files))
+
+    match len(files_grabbed):
+        case 0:
+            return
+        case 1:
+            return files_grabbed[0]
+        case other:
+            for i, file in enumerate(files_grabbed):
+                print("|", i + 1, "|", file)
+            choice = input(
+                "\nWhich file to open? Choose corresponding number: "
+            )
+            try:
+                os.system('cls||clear')
+                print(f"Opening {files_grabbed[int(choice) - 1]}")
+                return files_grabbed[int(choice) - 1]
+            except IndexError:
+                print("\nNumber outside of the list.")
+                return
+            except ValueError:
+                print("\nEmpty or wrong input.")
+                return
--- scenes_detection.py
+++ scenes_detection.py
@@ -1,38 +1,49 @@
 from scenedetect import open_video, SceneManager, AdaptiveDetector
 from scenedetect.scene_manager import save_images
+from files_operations import finding_video_file
+
+video_name = finding_video_file()
 
 
 def detecting_all_scenes():
-    video = open_video("TEST_B.mp4")
-    scene_manager = SceneManager()
-    scene_manager.add_detector(AdaptiveDetector())
-    scene_manager.detect_scenes(video=video, show_progress=True)
-    scene_list = scene_manager.get_scene_list(start_in_scene=True)
-    # print(scene_list)
+    if video_name is not None:
+        video = open_video(video_name)
+        scene_manager = SceneManager()
+        scene_manager.add_detector(AdaptiveDetector())
+        print("\n-Detecting scenes-")
+        scene_manager.detect_scenes(video=video, show_progress=True)
+        scene_list = scene_manager.get_scene_list(start_in_scene=True)
+        # print(scene_list)
 
-    # for i, scene in enumerate(scene_list):
-    #     print(
-    #         "Scene %2d: Start %s / Frame %d, End %s / Frame %d"
-    #         % (
-    #             i + 1,
-    #             scene[0].get_timecode(),
-    #             scene[0].get_frames(),
-    #             scene[1].get_timecode(),
-    #             scene[1].get_frames(),
-    #         )
-    #     )
+        # for i, scene in enumerate(scene_list):
+        #     print(
+        #         "Scene %2d: Start %s / Frame %d, End %s / Frame %d"
+        #         % (
+        #             i + 1,
+        #             scene[0].get_timecode(),
+        #             scene[0].get_frames(),
+        #             scene[1].get_timecode(),
+        #             scene[1].get_frames(),
+        #         )
+        #     )
 
-    return scene_list
+        return scene_list
 
 
 def creating_scenes_pictures(scene_list):
-    video = open_video("TEST_B.mp4")
-    img_paths = save_images(
-        scene_list=scene_list,
-        video=video,
-        show_progress=True,
-        encoder_param=20,
-        output_dir="./Scene_pictures",
-    )
+    if video_name is not None:
+        video = open_video(video_name)
+        print("\n-Saving pictures from scenes-")
+        img_paths = save_images(
+            scene_list=scene_list,
+            video=video,
+            show_progress=True,
+            encoder_param=20,
+            output_dir="./Scene_pictures",
+        )
 
-    return img_paths
+        return img_paths
+
+
+# scene_list = detecting_all_scenes()
+# img_paths = creating_scenes_pictures(scene_list)
