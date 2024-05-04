fb14d172a33a454c839863a7ccd2a263d45137ae
PiotrWeppo <piotr.weppo@gmail.com>
2024-05-04 13:45:59 +0200

    Added logging to all function for debugging purposes. Created main function

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+import logging
+import sys
 from text_recognition import (
     generate_imgs_with_text_from_video,
     check_if_vfx_in_found_scenes,
@@ -15,32 +17,50 @@ from files_operations import (
 )
 from scenes_detection import detect_all_scenes
 from xlsx_creator import create_dataframe, create_xlsx_file
-import sys
+from info_logger import start_logging_info
 
-print("Welcome to the VFX/ADR text detection program.\n")
-video = find_video_file()
-start_frame = set_video_start_time(video)
-create_folder("./temp/text_imgs", "./temp/tc_imgs", "./temp/thumbnails")
-frames_with_embedded_text_id = generate_imgs_with_text_from_video(
-    video, start_frame
-)
-scene_list = detect_all_scenes(video)
-# print(scene_list)
-frames_ranges_with_vfx_text = check_if_vfx_in_found_scenes(
-    scene_list, frames_with_embedded_text_id
-)
-# print(frames_with_embedded_text_id)
-generate_thumbnails_for_each_scene(video, frames_ranges_with_vfx_text)
-found_vfx_text = generate_vfx_text(frames_ranges_with_vfx_text, video)
-# print(found_vfx_text)
-found_adr_text = generate_adr_text(frames_with_embedded_text_id, video)
-# print(found_adr_text)
-merged_text_dict = merge_dicts(found_vfx_text, found_adr_text)
-# print(merged_text_dict)
-final_text_dict = add_real_timestamps(merged_text_dict, video)
-# print(final_text_dict)
-df = create_dataframe(final_text_dict)
-create_xlsx_file(df, video)
-delete_folder("./temp")
-input("Done.\n\nPress Enter to exit: ")
-sys.exit()
+
+def main():
+    print("Welcome to the VFX/ADR text detection program.\n")
+    video = find_video_file()
+    start_logging_info(video)
+    start_frame = set_video_start_time(video)
+    create_folder("./temp/text_imgs", "./temp/tc_imgs", "./temp/thumbnails")
+    frames_with_embedded_text_id = generate_imgs_with_text_from_video(
+        video, start_frame
+    )
+    logging.debug(f'frames_with_embedded_text_id=\n{frames_with_embedded_text_id}\n')
+    scene_list = detect_all_scenes(video)
+    loggers = [logging.getLogger(name) for name in logging.root.manager.loggerDict]
+    print(loggers)
+    logging.debug(f'scene_list=\n{scene_list}\n')
+    # print(scene_list)
+    frames_ranges_with_vfx_text = check_if_vfx_in_found_scenes(
+        scene_list, frames_with_embedded_text_id
+    )
+    logging.debug(f'frames_ranges_with_vfx_text=\n{frames_ranges_with_vfx_text}\n')
+    # print(frames_with_embedded_text_id)
+    generate_thumbnails_for_each_scene(video, frames_ranges_with_vfx_text)
+    found_vfx_text = generate_vfx_text(frames_ranges_with_vfx_text, video)
+    logging.debug(f'found_vfx_text=\n{found_vfx_text}\n')
+    found_adr_text = generate_adr_text(frames_with_embedded_text_id, video)
+    logging.debug(f'found_adr_text=\n{found_adr_text}\n')
+    merged_text_dict = merge_dicts(found_vfx_text, found_adr_text)
+    logging.debug(f'merged_text_dict=\n{merged_text_dict}\n')
+    final_text_dict = add_real_timestamps(merged_text_dict, video)
+    logging.debug(f'final_text_dict=\n{final_text_dict}\n')
+    df = create_dataframe(final_text_dict)
+    create_xlsx_file(df, video)
+    delete_folder("./temp")
+    input("Done.\n\nPress Enter to exit: ")
+    sys.exit()
+
+
+if __name__ == "__main__":
+    try:
+        main()
+    except Exception as e:
+        logging.exception("Main crashed. Error below:\n\n%s", e)
+        print("An error occurred. Check the log file for more information.")
+        input("Press Enter to exit: ")
+        sys.exit()
